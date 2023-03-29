533abd1ad0d3d82b92f4878377c64096929a564b
PhanDuc2k3 <21011596@st.phenikaa-uni.edu.vn>
2023-03-29 22:09:29 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
+
+  const numberList = [1, 2, 3, 4, 5];
+
+ 
+  const user = {
+    name: 'Name',
+    age: 0,
+    email: 'ex@gmail.com',
+  };
+
+
+  const userList = [
+    {
+      name: 'PhanDuc',
+      age: 20,
+      email: 'PhanDuc@gmail.com',
+    },
+    {
+      name: 'MinhDuc',
+      age: 19,
+      email: 'MinhDuc@gmail.com',
+    },
+  ];
+
+
+  const renderNumberList = () => {
+    return (
+      <ul>
+        {numberList.map((number) => (
+          <li key={number}>{number}</li>
+        ))}
+      </ul>
+    );
+  };
+
+ 
+  const renderUser = () => {
+    return (
+      <div>
+        <p>Name: {user.name}</p>
+        <p>Age: {user.age}</p>
+        <p>Email: {user.email}</p>
+      </div>
+    );
+  };
+
+ 
+  const renderUserList = () => {
+    return (
+      <ul>
+        {userList.map((user) => (
+          <li key={user.email}>
+            <p>Name: {user.name}</p>
+            <p>Age: {user.age}</p>
+            <p>Email: {user.email}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {renderNumberList()}
+      {renderUser()}
+      {renderUserList()}
     </div>
   );
 }
